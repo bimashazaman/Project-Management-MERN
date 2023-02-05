85f7d5aa0f366a1d1154d8f029f31c1afa045eb1
bimashazaman <bimashazaman1234.com>
2023-02-05 07:30:57 +0600

    Header

--- client/src/components/partials/Header.jsx
+++ client/src/components/partials/Header.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
-const Header = () => {
-  return <div>Header</div>
+export default function Header() {
+  return (
+    <nav className='navbar bg-light mb-4 p-0'>
+      <div className='container'>
+        <a className='navbar-brand' href='/'>
+          <div className='d-flex'>Project Management</div>
+        </a>
+      </div>
+    </nav>
+  )
 }
-
-export default Header
