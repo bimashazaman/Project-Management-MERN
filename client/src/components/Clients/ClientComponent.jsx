f091b80e08e58b3f46b54aad7d67f07530737860
bimashazaman <bimashazaman1234.com>
2023-02-05 07:44:51 +0600

    Getting all the data in the frontend by apollo

--- client/src/components/Clients/ClientComponent.jsx
+++ client/src/components/Clients/ClientComponent.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_CLIENTS = gql`
+  query getClients {
+    clients {
+      id
+      name
+      email
+      phone
+    }
+  }
+`
 
 const ClientComponent = () => {
-  return <div>hi</div>
+  const { data, loading, error } = useQuery(GET_CLIENTS)
+
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
+  console.log(data)
+
+  return (
+    <div>
+      {!loading &&
+        !error &&
+        data.clients.map((client) => (
+          <div key={client.id}>
+            <p>{client.name}</p>
+            <p>{client.email}</p>
+            <p>{client.phone}</p>
+          </div>
+        ))}
+    </div>
+  )
 }
 
 export default ClientComponent
