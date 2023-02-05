41affe30d357ee2a16ff8215f99a0a9c5ac6dd80
bimashazaman <bimashazaman1234.com>
2023-02-05 07:35:02 +0600

    ApolloClient setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import Layout from './components/layout/Layout'
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:3000/graphql',
+  }),
+})
 
 function App() {
-  return <Layout>hi</Layout>
+  return (
+    <ApolloProvider client={client}>
+      <Layout> </Layout>
+    </ApolloProvider>
+  )
 }
 
 export default App
